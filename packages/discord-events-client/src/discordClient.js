8b42d38422240fe47ee32ed125fe065e05b005f0
Kevin Partington <platinumazure@gmail.com>
2022-11-25 16:29:41 -0600

    feat(bot): create initial login logic

--- packages/discord-events-client/src/discordClient.js
+++ packages/discord-events-client/src/discordClient.js
@@ -1,1 +1,10 @@
-export const DiscordClient = {};
+import { Client } from "discord.js";
+import { GatewayIntentBits } from "discord-api-types/v10";
+
+const client = new Client({ intents: [GatewayIntentBits.GuildScheduledEvents] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(); // Uses process.env.DISCORD_TOKEN
