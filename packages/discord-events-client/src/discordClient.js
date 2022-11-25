6a866c1e0c29277c23df7a048e2170063896550e
Kevin Partington <platinumazure@gmail.com>
2022-11-25 17:18:13 -0600

    feat(bot): initial test for retrieving guilds and events

--- packages/discord-events-client/src/discordClient.js
+++ packages/discord-events-client/src/discordClient.js
@@ -1,10 +1,29 @@
 import { Client } from "discord.js";
 import { GatewayIntentBits } from "discord-api-types/v10";
 
-const client = new Client({ intents: [GatewayIntentBits.GuildScheduledEvents] });
+const client = new Client({
+    intents: [GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.Guilds]
+});
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
+
+    client.guilds.fetch()
+        .then(guilds => {
+            guilds.forEach(guild => {
+                guild.fetch()
+                    .then(g => g.scheduledEvents.fetch()
+                        .then(events => {
+                            console.log(`${guild}: ${events.size} event(s) found`);
+                            events.forEach(e => {
+                                console.log(`\t- ${e.name} (${e.scheduledStartAt})`);
+                            });
+                        })
+                        .catch(err => console.log(`${guild}: Could not retrieve events: ${err}`)))
+                    .catch(err => console.log(`${guild}: Could not hydrate guild: ${err}`));
+            });
+        })
+        .catch(err => console.log(`Could not retrieve guilds: ${err}`));
 });
 
 client.login(); // Uses process.env.DISCORD_TOKEN
